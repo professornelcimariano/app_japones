5ea062fb5d4e59863645d5e94212515c585ba684
Nelci <professornelcimariano@gmail.com>
2024-09-25 17:29:33 -0300

    alterações pc senac

--- app/products.js
+++ app/products.js
@@ -6,28 +6,36 @@ import axios from 'axios'; // Import axios
 
 export default function ProductsList() {
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
     axios.get(API_URL + '/products')
       .then(response => {
         setProducts(response.data.list);
       })
-      .catch(error => console.error('Erro ao carregar Produtos:', error));
+      .catch(error => {
+        console.error('Erro ao carregar Produtos:', error);
+        alert('Erro ao carregar Produtos: ' + error.message); // Exibe o erro no celular
+      });
   }, []);
   return (
     <ScrollView contentContainerStyle={styles.container}>
-    {products.map(item => (
-      <View key={item.id}>
-        <View>
-          <Text style={{ fontSize: 18, fontWeight: 'bold'}}>{item.title}</Text>
+      {products.map(item => (
+        <View key={item.id}>
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{item.title}</Text>
+          </View>
+          <View>
+            <Text style={{ textAlign: 'justify' }}>{item.description}</Text>
+          </View>
+          <View style={{
+            height: 1, backgroundColor: '#ccc', marginTop: 20,
+            width: '80%', alignSelf: 'center'
+          }}> </View>
         </View>
-        <View>
-          <Text style={{ textAlign: 'justify' }}>{item.description}</Text>
-        </View>
-        <View style={{ height: 1, backgroundColor: '#ccc', marginTop: 20, width: '80%', alignSelf: 'center' }} />
-      </View>
-    ))}
+      ))
+      }
 
-  </ScrollView>
+    </ScrollView >
   )
 }
 const styles = StyleSheet.create({
--- back-end/app.js
+++ back-end/app.js
@@ -16,58 +16,58 @@ app.use((req, res, next) => {
 
 //http://localhost:4000
 app.get('/', (req, res) => {
-    res.send('Rota Inicial do Projeto');
+  res.send('Rota Inicial do Projeto');
 });
 //http://localhost:4000/bemvindo
 app.get("/bemvindo", (req, res) => {
-    res.send("Bem vindo Desenvolvedor");
+  res.send("Bem vindo Desenvolvedor");
 });
 
 //Retorno com json
 //http://localhost:4000/json
 app.get("/json", function (req, res) {
-    return res.json({
-        course: 'Node Js',
-        title: 'Desenvolvimento de API',
-        description: 'Aprenda a desenvolver uma API com Node Js',
-        date: '2024-07-20'
-    })
+  return res.json({
+    course: 'Node Js',
+    title: 'Desenvolvimento de API',
+    description: 'Aprenda a desenvolver uma API com Node Js',
+    date: '2024-07-20'
+  })
 });
 
 // Query Params - parâmetros passados na frente da rota, na URL
 //http:localhost:4000/curso?nome=Javascript
 app.get("/curso", function (req, res) {
-    let x = req.query.sobrenome;
-    return res.json({ curso: `Aprendendo ${x}` });
-  }); 
+  let x = req.query.sobrenome;
+  return res.json({ curso: `Aprendendo ${x}` });
+});
 
 // Route Params - parâmetros passados via URL com a barra (/)  
 //http:localhost:4000/turma/1
-  app.get("/turma/:id", function (req, res) {
-    let id = req.params.id;
-    return res.json({ turma: `Turma: ${id}` });
-  }); 
+app.get("/turma/:id", function (req, res) {
+  let id = req.params.id;
+  return res.json({ turma: `Turma: ${id}` });
+});
 
 //Request Body - parâmetros passados no corpo da requisição
 //http://localhost:4000/contato/5?sit=ativo
-  app.get("/contato/:id", function (req, res) {
-    let id = req.params.id;
-    let sit = req.query.sit;
+app.get("/contato/:id", function (req, res) {
+  let id = req.params.id;
+  let sit = req.query.sit;
   return res.json({ id, sit, nome: 'Nelci', email: 'professornelcimariano@gmail.com' });
-  }); 
+});
 
 
 let cursos = ['Node JS', 'Javascript', 'React'];
 //Retorno de Todos os Cursos -> http://localhost:4000/cursos
 app.get("/cursos", function (req, res) {
   return res.json(cursos);
-}); 
+});
 
 //Retorno de 1 curso específico -> http://localhost:4000/cursos/1
 app.get("/cursos/:index", function (req, res) {
-  const {index} = req.params;
+  const { index } = req.params;
   return res.json(cursos[index]);
-}); 
+});
 
 
 // Requisição com axios
@@ -82,9 +82,10 @@ app.use(express.urlencoded({ extended: true }));
 // crie a const axios na parte de cima -> const axios = require('axios');
 // http://localhost:4000/produtos
 app.get("/products", function (req, res) {
-  axios.get('http://localhost:'+port+'/products.json')
+  axios.get('http://localhost:' + port + '/products.json')
+  // axios.get('http://10.55.49.38:4000/products.json')
     .then(response => {
-      res.json(response.data);      
+      res.json(response.data);
     })
     .catch(error => {
       res.status(500).send('Erro ao ler o arquivo');
@@ -108,7 +109,7 @@ response.config: a configuração que foi usada para fazer a solicitação.
 //Retorna um produto específico
 //http://localhost:4000/produtos/1
 app.get("/products/:id", function (req, res) { // Define uma rota que recebe um parâmetro id
-  axios.get('http://localhost:'+port+'/products.json') // Faz uma solicitação GET para o arquivo products.json
+  axios.get('http://localhost:' + port + '/products.json') // Faz uma solicitação GET para o arquivo products.json
     .then(response => { // função de callback que é chamada quando a promessa retornada. A resposta da solicitação é passada para esta função.
       // const {listProduct} = response.data; //extrai a lista de produtos dos dados da resposta.
       const listProduct = response.data.listProduct; //extrai a lista de produtos dos dados da resposta.
@@ -128,13 +129,13 @@ app.get("/products/:id", function (req, res) { // Define uma rota que recebe um 
 // http://localhost:4000/produtos/search?q=sushi
 app.get("/products/search", function (req, res) {
   const query = req.query.q; // Obtém o parâmetro de consulta 'q' da URL
-  
+
   // console.log('Query recebida:', query); // Log da query recebida
   // Faz uma requisição GET para o arquivo JSON
   axios.get('http://localhost:4000/products.json')
     .then(response => {
       const { listProduct } = response.data; // Extrai a lista de produtos dos dados da resposta
-      
+
       // Faz a busca por palavra-chave no título ou descrição
       const filteredProducts = listProduct.filter(product =>
         product.title.toLowerCase().includes(query.toLowerCase()) || // Verifica se o título contém a palavra
@@ -156,5 +157,5 @@ app.get("/products/search", function (req, res) {
 
 
 app.listen(port, () => {
-    console.log('Servidor Iniciado na porta');
+  console.log('Servidor Iniciado na porta');
 }); 
