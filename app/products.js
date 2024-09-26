bd8dce002add06be408e1c64aede1a9e1dca977d
Nelci Mariano <professornelcimariano@gmail.com>
2024-09-26 09:31:47 -0300

    Alterações no Projeto para resolução de exibição dos dados de Products no celular

--- app/products.js
+++ app/products.js
@@ -6,31 +6,23 @@ import axios from 'axios'; // Import axios
 
 export default function ProductsList() {
   const [products, setProducts] = useState([]);
-
   useEffect(() => {
     axios.get(API_URL + '/products')
       .then(response => {
         setProducts(response.data.list);
       })
-      .catch(error => {
-        console.error('Erro ao carregar Produtos:', error);
-        alert('Erro ao carregar Produtos: ' + error.message); // Exibe o erro no celular
-      });
+      .catch(error => console.error('Erro ao carregar Products:', error));
   }, []);
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {products.map(item => (
         <View key={item.id}>
           <View>
-            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{item.title}</Text>
+            <Text style={styles.title}>{item.title}</Text>
           </View>
           <View>
-            <Text style={{ textAlign: 'justify' }}>{item.description}</Text>
+            <Text style={styles.description}>{item.description}</Text>
           </View>
-          <View style={{
-            height: 1, backgroundColor: '#ccc', marginTop: 20,
-            width: '80%', alignSelf: 'center'
-          }}> </View>
         </View>
       ))
       }
@@ -44,6 +36,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 15
+  }, 
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  description: {
+    textAlign: 'justify',
+    marginBottom: 10
   }
-
 });
